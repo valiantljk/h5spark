b07be91f2082d7a5107d2355d6c89048b1284506
Jialin Liu <jalnliu@lbl.gov>
2016-01-04 15:37:38 -0800

    load multiple hdf5 files

--- test/h5py-spark.py
+++ test/h5py-spark.py
@@ -3,28 +3,21 @@ import sys
 from pyspark import SparkContext
 
 if __name__ == "__main__":
-  """
-  Usage: doit [partitions]
-  """
+  #Usage: doit [partitions]
+
   sc = SparkContext(appName="SparkHDF5")
   partitions = int(sys.argv[1]) if len(sys.argv) > 1 else 2
 
- # read a dataset and return it as a Python list #
+  # read a dataset and return it as a Python list
+  def f(x):
+    a = x.split(",")
+    with h5py.File(a[0], 'r') as f:
+        result = f['autoencoded']
+        return list(result[:,0])
+  file_paths = sc.textFile("../resources/hdf5/file_names_and_paths.csv", minPartitions=partitions)
 
- def f(x):
-     a = x.split(",")
-     with h5py.File(a[0]) as f:
-         result = f[a[1]]
-         return list(result[:])
+  rdd = file_paths.flatMap(f)
+  print "\n count %d : min %f : mean %f : stdev %f : max %f\n" % (rdd.count(), rdd.min(), rdd.mean(),rdd.stdev(), rdd.max())
 
-
- file_paths = sc.textFile("file_names_and_paths.csv", minPartitions=partitions)
-
- rdd = file_paths.flatMap(f)
-
- print "\ncount %d : min %f : mean %f : stdev %f : \
-     max %f\n" % (rdd.count(), rdd.min(), rdd.mean(), \
-                           rdd.stdev(), rdd.max())
-
- print rdd.histogram(10)
- sc.stop()
+  print rdd.histogram(10)
+  sc.stop()

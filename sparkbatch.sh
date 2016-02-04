0e1a902ac5d54156caecd381c5c792dfa554484f
valiantljk <jaln.liu@ttu.edu>
2016-02-04 13:49:00 -0800

    update

---
+++ sparkbatch.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+#SBATCH -p debug
+#SBATCH -N 2
+#SBATCH -t 00:30:00
+#SBATCH -e mysparkjob_%j.err
+#SBATCH -o mysparkjob_%j.out
+#SBATCH --ccm
+module unload spark/hist-server
+module load spark
+module unload python
+module load python/2.7-anaconda
+
+ 
+start-all.sh
+
+spark-submit --master $SPARKURL --driver-memory 15G --executor-memory 32G --class org.apache.spark.mllib.linalg.distributed.netCDFTest --conf spark.eventLog.enabled=true --conf spark.eventLog.dir=$SCRATCH/spark/spark_event_logs ./test/h5kread.py
+
+#module load spark/hist-server
+#./run_history_server.sh $EVENT_LOGS_DIR 
+
+stop-all.sh

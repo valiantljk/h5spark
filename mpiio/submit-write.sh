5b72227c0f8652a1f776fa78fbc4282729401035
valiantljk <jaln.liu@ttu.edu>
2016-03-25 02:41:13 -0700

    update

---
+++ mpiio/submit-write.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH -p debug 
+#SBATCH -N 100
+#SBATCH -t 00:30:00
+#SBATCH -J h5write
+#SBATCH -e %j.err
+#SBATCH -o %j.out
+
+##arguments: 
+## -i: inputfilename, -o:outputfilename, -b: collective_buffersize, -n: collective_buffernodes, -k: iscollective, -v: datasetname
+
+##Test local 1.h5, change "SBATCH -N 25" to "SBATCH -N 1"
+##srun -n 10 ./h5read -f 1.h5 -b 16777216 -n 2 -k 1  -v inputs 
+
+##Test Alex's 2TB
+#module load cray-hdf5-parallel
+#srun -n 1600 ./h5read -f /global/cscratch1/sd/gittens/CFSROhdf5/oceanTemps.hdf5  -b 16777216 -n 50 -k 0 -v temperatures
+#srun -n 1200 ./h5move -i /global/cscratch1/sd/gittens/CFSROhdf5/oceanTemps.hdf5 -o /global/cscratch1/sd/jialin/climate/temp2.h5 -b 16777216 -n 50 -k 0 -v temperatures
+
+
+srun -n 3200 ./h5write -f /scratch1/scratchdirs/jialin/hdf-data/test3.h5 -b 16777216 -n 100 -k 1 -x 3200000 -y 300000 

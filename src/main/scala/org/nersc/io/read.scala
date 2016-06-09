e04a06f42e8e04249797eab92b4ac2f7b530c730
Alex Gittens <gittens@icsi.berkeley.edu>
2016-06-09 16:27:48 -0700

    read should return arrays with the correct number of rows now

--- src/main/scala/org/nersc/io/read.scala
+++ src/main/scala/org/nersc/io/read.scala
@@ -185,11 +185,9 @@ object read {
 
   private def read_array(FILENAME: String, DATASETNAME: String, start: Long, end: Long): (Array[Array[Double]]) = {
     val dset_dims: Array[Long] = getdimensions(FILENAME, DATASETNAME)
+    val end1 = if (end > dset_dims(0) ) dset_dims(0) else end
     var (dset_datas: Array[Double], index: Array[Long]) = read_hyperslab(FILENAME, DATASETNAME, start, end)
-    var dset_data: Array[Array[Double]] = Array.ofDim((end - start).toInt, index.length/(end-start).toInt)
-    var end1 = end
-    if (end1 > dset_dims(1))
-      end1 = dset_dims(1)
+    var dset_data: Array[Array[Double]] = Array.ofDim((end1 - start).toInt, index.length/(end1-start).toInt)
     for (id <- 0 to (end1 - start).toInt - 1) {
       for (jd <- 0 to dset_dims(1).toInt - 1) {
         dset_data(id)(jd) = dset_datas(id * dset_dims(1).toInt + jd)

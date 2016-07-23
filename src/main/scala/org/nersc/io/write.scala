c1f78ed56957283b29a6ffc8e926a144e7848bea
valiantljk <valiantljk@gmail.com>
2016-07-22 17:03:16 -0700

    update design note

--- src/main/scala/org/nersc/io/write.scala
+++ src/main/scala/org/nersc/io/write.scala
@@ -20,9 +20,17 @@ import scala.collection.immutable.NumericRange
 
 
 object write {
-  
-  def h5write_array(sc: SparkContext, inpath: File, variable: String, partitions: Long,content: RDD[Double],overwrite: Boolean):Boolean = {
+  /* the h5write_array take one RDD partition
+   * the structure of RDD is essential for this function, e.g., it can be RDD[Array[Double]], or RDD[value: Double, index: Long]
+   * and figure out the hyperslab for writing that partition into the hdf5 file 
+   * h5write_2d is to convert the RDD into a 2D dataset in HDF5. 
+   */
+
+
+  //def h5write_1d()
+  def h5write(sc:SparkContext,inpath:File,variable:String, partitions: Long, content: RDD[Array[Double],Long], overwrite: Boolean])
+  def h5write_2d(sc: SparkContext, inpath: File, variable: String, partitions: Long,content: RDD[Double],overwrite: Boolean):Boolean = {
     true
- }
-   
+  }
+  //def h5write_3d(){} 
 }
